fb2277ed41a3bf6af6a5d6f705f0fe6c3a14c0f6
Yong Yuan <yongyuanstu@gmail.com>
2015-08-19 08:34:58 +0800

    Update imgNamesToTXT.py

--- bag-of-words-stable-version/imgNamesToTXT.py
+++ bag-of-words-stable-version/imgNamesToTXT.py
@@ -18,7 +18,7 @@ images_names = os.listdir(imagesDir)
 
 f = open("imageNamesList.txt", "w")
 for ind, str_each_file in enumerate(images_names):
-    image_full_path = '/'.join([imagesDir, str_each_file])
+    image_full_path = ''.join([imagesDir, str_each_file])
     print 'wirte %s' % (image_full_path)
     f.writelines('%s\n' % image_full_path)
 f.close
